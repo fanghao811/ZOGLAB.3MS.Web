437e42df7998faae4f272385b8dadb6dcac66bb2
方豪 <fanghao811@live.com>
2018-12-07 18:15:56 +0800

    Added_TreeUnitManager

--- ZOGLAB.S_3MS.Core/SD/SD_TreeUnit/TreeUnitManager.cs
+++ ZOGLAB.S_3MS.Core/SD/SD_TreeUnit/TreeUnitManager.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.Collections.Extensions;
 using Abp.Domain.Repositories;
 using Abp.Domain.Services;
 using Abp.Domain.Uow;
+using Abp.Extensions;
 using Abp.UI;
 using Abp.Zero;
 
@@ -46,10 +49,10 @@ namespace ZOGLAB.S_3MS.SD
             if (lastChild == null)
             {
                 var parentCode = parentId != null ? await GetCodeAsync(parentId.Value) : null;
-                return TreeUnit.AppendCode(parentCode, TreeUnit.CreateCode(1));
+                return AppendCode(parentCode, CreateCode(1));
             }
 
-            return TreeUnit.CalculateNextCode(lastChild.Code);
+            return CalculateNextCode(lastChild.Code);
         }
 
         public virtual async Task<TreeUnit> GetLastChildOrNullAsync(long? parentId)
@@ -100,7 +103,7 @@ namespace ZOGLAB.S_3MS.SD
             //Update Children Codes
             foreach (var child in children)
             {
-                child.Code = TreeUnit.AppendCode(treeUnit.Code, TreeUnit.GetRelativeCode(child.Code, oldCode));
+                child.Code = AppendCode(treeUnit.Code,GetRelativeCode(child.Code, oldCode));
             }
         }
 
@@ -133,6 +136,124 @@ namespace ZOGLAB.S_3MS.SD
             {
                 throw new UserFriendlyException(L("TreeUnitDuplicateDisplayNameWarning", treeUnit.DisplayName));
             }
+        }
+
+
+        /// <summary>
+        /// Creates code for given numbers.
+        /// Example: if numbers are 4,2 then returns "00004.00002";
+        /// </summary>
+        /// <param name="numbers">Numbers</param>
+        public static string CreateCode(params int[] numbers)
+        {
+            if (numbers.IsNullOrEmpty())
+            {
+                return null;
+            }
+
+            return numbers.Select(number => number.ToString(new string('0', TreeUnit.CodeUnitLength))).JoinAsString(".");
+        }
+
+        /// <summary>
+        /// Appends a child code to a parent code. 
+        /// Example: if parentCode = "00001", childCode = "00042" then returns "00001.00042".
+        /// </summary>
+        /// <param name="parentCode">Parent code. Can be null or empty if parent is a root.</param>
+        /// <param name="childCode">Child code.</param>
+        public static string AppendCode(string parentCode, string childCode)
+        {
+            if (childCode.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(childCode), "childCode can not be null or empty.");
+            }
+
+            if (parentCode.IsNullOrEmpty())
+            {
+                return childCode;
+            }
+
+            return parentCode + "." + childCode;
+        }
+
+        /// <summary>
+        /// Gets relative code to the parent.
+        /// Example: if code = "00019.00055.00001" and parentCode = "00019" then returns "00055.00001".
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <param name="parentCode">The parent code.</param>
+        public static string GetRelativeCode(string code, string parentCode)
+        {
+            if (code.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(code), "code can not be null or empty.");
+            }
+
+            if (parentCode.IsNullOrEmpty())
+            {
+                return code;
+            }
+
+            if (code.Length == parentCode.Length)
+            {
+                return null;
+            }
+
+            return code.Substring(parentCode.Length + 1);
+        }
+
+        /// <summary>
+        /// Calculates next code for given code.
+        /// Example: if code = "00019.00055.00001" returns "00019.00055.00002".
+        /// </summary>
+        /// <param name="code">The code.</param>
+        public static string CalculateNextCode(string code)
+        {
+            if (code.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(code), "code can not be null or empty.");
+            }
+
+            var parentCode = GetParentCode(code);
+            var lastUnitCode = GetLastUnitCode(code);
+
+            return AppendCode(parentCode, CreateCode(Convert.ToInt32(lastUnitCode) + 1));
+        }
+
+        /// <summary>
+        /// Gets the last unit code.
+        /// Example: if code = "00019.00055.00001" returns "00001".
+        /// </summary>
+        /// <param name="code">The code.</param>
+        public static string GetLastUnitCode(string code)
+        {
+            if (code.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(code), "code can not be null or empty.");
+            }
+
+            var splittedCode = code.Split('.');
+            return splittedCode[splittedCode.Length - 1];
+        }
+
+        /// <summary>
+        /// Gets parent code.
+        /// Example: if code = "00019.00055.00001" returns "00019.00055".
+        /// </summary>
+        /// <param name="code">The code.</param>
+        public static string GetParentCode(string code)
+        {
+            if (code.IsNullOrEmpty())
+            {
+                throw new ArgumentNullException(nameof(code), "code can not be null or empty.");
+            }
+
+            var splittedCode = code.Split('.');
+            if (splittedCode.Length == 1)
+            {
+                return null;
+            }
+
+            return splittedCode.Take(splittedCode.Length - 1).JoinAsString(".");
         }
     }
 }
